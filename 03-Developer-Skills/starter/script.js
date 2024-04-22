5c0003443efc0f215c8ecfb081df0781da80a10e
paramveer665 <paramveerchauhan665@gmail.com>
2024-04-23 00:51:05 +0530

    work done

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,27 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+console.log('hello');
+
+//temprature amplitude finder
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+let high = 0;
+let low = 0;
+const calcTempAmp = function () {
+  for (let i = 0; i < temperatures.length; i++) {
+    if (temperatures[i] == 'error') {
+      continue;
+    } else if (temperatures[i] > high) {
+      high = temperatures[i];
+    } else if (temperatures[i] < low) {
+      low = temperatures[i];
+    }
+  }
+};
+calcTempAmp();
+console.log(`Amplitude in temprature is ${high - low}`);
+
+console.log(temperatures);
+
+console.log(`High temp is ${high} and low temp is ${low}`);
