fd1aa20284c97bb6ee75ad537620e8724576def1
paramveer665 <paramveerchauhan665@gmail.com>
2024-04-27 01:12:31 +0530

    guess the number completed

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const num = Math.trunc(Math.random() * 20) + 1;
-
-let score = 20;
-
+let num = Math.trunc(Math.random() * 20) + 1;
 console.log(num);
+let score = 20;
+let highScore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessVal = Number(document.querySelector('.guess').value);
 
   if (!guessVal) {
     document.querySelector('.message').textContent = '⛔ Insert a number';
-  } else if (guessVal > num) {
+  }
+  ///if guess is high
+  else if (guessVal > num) {
     document.querySelector('.message').textContent =
       ' 📈 Guess number is too high';
     score--;
@@ -19,6 +21,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent =
         '💥You have lost the game';
     }
+
+    ///if guess is low
   } else if (guessVal < num) {
     document.querySelector('.message').textContent =
       '📉 Guess number is too low';
@@ -29,10 +33,30 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent =
         '💥You have lost the game';
     }
+    ///If guessed correctly
   } else {
     document.querySelector('.message').textContent = ' 🥳 Guessed Correctly';
     document.querySelector('.number').textContent = num;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
+});
+
+///again button
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start guessing again...';
+  document.querySelector('.score').textContent = '20';
+  document.querySelector('body').style.backgroundColor = '#222222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = 0;
+  num = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  console.log(num);
 });
