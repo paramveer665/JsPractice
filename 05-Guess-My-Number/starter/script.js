63455e8f7707b9d1df2bb949cf24572ffcaada39
paramveer665 <paramveerchauhan665@gmail.com>
2024-04-27 00:07:32 +0530

    game logic is ready

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,35 @@
 "use strict";
+
+const num = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+console.log(num);
 document.querySelector(".check").addEventListener("click", function () {
   const guessVal = Number(document.querySelector(".guess").value);
 
   if (!guessVal) {
     document.querySelector(".message").textContent = "⛔ Insert a number";
+  } else if (guessVal > num) {
+    document.querySelector(".message").textContent =
+      " 📈 Guess number is too high";
+    score--;
+    document.querySelector(".score").textContent = score;
+    if (score < 1) {
+      document.querySelector(".message").textContent =
+        "💥You have lost the game";
+    }
+  } else if (guessVal < num) {
+    document.querySelector(".message").textContent =
+      "📉 Guess number is too low";
+    score--;
+    document.querySelector(".score").textContent = score;
+
+    if (score < 1) {
+      document.querySelector(".message").textContent =
+        "💥You have lost the game";
+    }
+  } else {
+    document.querySelector(".message").textContent = " 🥳 Guessed Correctly";
   }
-  // else if(guess>){
-  //   docu
-  // }
 });
