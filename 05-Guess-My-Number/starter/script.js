8986ee7e5a3277b6939361429b4725a18b19e256
paramveer665 <paramveerchauhan665@gmail.com>
2024-04-27 00:33:32 +0530

    towards the completion

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,36 +1,38 @@
-"use strict";
+'use strict';
 
 const num = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
 console.log(num);
-document.querySelector(".check").addEventListener("click", function () {
-  const guessVal = Number(document.querySelector(".guess").value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guessVal = Number(document.querySelector('.guess').value);
 
   if (!guessVal) {
-    document.querySelector(".message").textContent = "⛔ Insert a number";
+    document.querySelector('.message').textContent = '⛔ Insert a number';
   } else if (guessVal > num) {
-    document.querySelector(".message").textContent =
-      " 📈 Guess number is too high";
+    document.querySelector('.message').textContent =
+      ' 📈 Guess number is too high';
     score--;
-    document.querySelector(".score").textContent = score;
+    document.querySelector('.score').textContent = score;
     if (score < 1) {
-      document.querySelector(".message").textContent =
-        "💥You have lost the game";
+      document.querySelector('.message').textContent =
+        '💥You have lost the game';
     }
   } else if (guessVal < num) {
-    document.querySelector(".message").textContent =
-      "📉 Guess number is too low";
+    document.querySelector('.message').textContent =
+      '📉 Guess number is too low';
     score--;
-    document.querySelector(".score").textContent = score;
+    document.querySelector('.score').textContent = score;
 
     if (score < 1) {
-      document.querySelector(".message").textContent =
-        "💥You have lost the game";
+      document.querySelector('.message').textContent =
+        '💥You have lost the game';
     }
   } else {
-    document.querySelector(".message").textContent = " 🥳 Guessed Correctly";
-    document.querySelector(".number").textContent = num;
+    document.querySelector('.message').textContent = ' 🥳 Guessed Correctly';
+    document.querySelector('.number').textContent = num;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   }
 });
