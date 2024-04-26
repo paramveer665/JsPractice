4ead8a3b074f8d51896eee6f724fa81cc04cc440
paramveer665 <paramveerchauhan665@gmail.com>
2024-04-26 23:33:24 +0530

    new day

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+document.querySelector(".check").addEventListener("click", function () {
+  const guessVal = Number(document.querySelector(".guess").value);
+
+  if (!guessVal) {
+    document.querySelector(".message").textContent = "⛔ Insert a number";
+  }
+  // else if(guess>){
+  //   docu
+  // }
+});
