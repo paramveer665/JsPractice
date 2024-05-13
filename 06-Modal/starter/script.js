23bf77bc1f1456494b60d4072f476d8ba6703bb6
paramveer665 <paramveerchauhan665@gmail.com>
2024-05-14 01:37:29 +0530

    modal completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,9 +1,30 @@
 'use strict';
 const btnOpen = document.querySelectorAll('.show-modal');
 const btClose = document.querySelector('.close-modal');
-const overylay = document.querySelector('.overlay');
+const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 
-console.log(btnOpen);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnOpen.length; i++) console.log(btnOpen[i].textContent);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpen.length; i++) {
+  btnOpen[i].addEventListener('click', openModal);
+}
+
+btClose.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //   console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
