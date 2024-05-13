611c6fd3235c7a368e483efd8714c94f27700238
paramveer665 <paramveerchauhan665@gmail.com>
2024-05-14 00:40:19 +0530

    modal work

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const btnOpen = document.querySelectorAll('.show-modal');
+const btClose = document.querySelector('.close-modal');
+const overylay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+console.log(btnOpen);
+
+for (let i = 0; i < btnOpen.length; i++) console.log(btnOpen[i].textContent);
