f529b2bff9869bf36a4b17c0493d43c05e92f01b
paramveer665 <paramveerchauhan665@gmail.com>
2024-08-03 15:34:09 +0530

    mast kaam chal rha hai

---
+++ test.js
@@ -0,0 +1,37 @@
+let mydate = new Date();
+
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toDateString());
+
+// const myArr = [1, 2, 3, 4, 5];
+
+// const myHero = ["sanjay", "akshay", "henry cavil"];
+
+// console.log(myHero);
+
+// const myna1 = myArr.slice(1, 3);
+// console.log("A", myArr);
+
+// console.log(myna1);
+
+// const myna2 = myArr.splice(1, 3);
+
+// console.log("A", myArr);
+
+// console.log(myna2);
+// const mum = ["mausi", "mama", "nani", "nana"];
+
+// const dad = ["chacha", "tau", "dadi", "dada"];
+
+// mum.push(dad);
+
+// console.log(mum);
+
+let lname = "rahul";
+let age = 20;
+let designation = "clerk";
+console.log(Array.of(lname, age, designation));
+const naam = Array.from("Mera naam mukesh hai");
+
+console.log(Array.isArray(naam));
