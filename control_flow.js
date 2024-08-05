34c7976405b6e03b0c8d3779dfba31f28337916c
paramveer665 <paramveerchauhan665@gmail.com>
2024-08-06 00:01:19 +0530

    basic done,followng chai or code

---
+++ control_flow.js
@@ -0,0 +1,21 @@
+const user = {
+  name: "",
+  //   age:  24,
+  gender: "",
+};
+
+const studentMarks = [30, 26, 37, 32];
+
+// if (Object.keys(user).length === 0) {
+//   console.log("object is empty");
+// } else if (Object.values(user).length > 0) {
+//   console.log(`Object contains " ${Object.values(user)} " attributes`);
+// }
+
+// if (studentMarks.length === 0) {
+//   console.log("no marks present in database for user");
+// } else {
+//   console.log(`Users marks are ${studentMarks}`);
+// }
+
+console.log(null == 0);
