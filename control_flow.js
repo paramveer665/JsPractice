87c939c6d252e5c9f322845e1fabb529b8384da8
paramveer665 <paramveerchauhan665@gmail.com>
2024-09-09 02:04:18 +0530

    basics done

--- control_flow.js
+++ control_flow.js
@@ -18,4 +18,69 @@ const studentMarks = [30, 26, 37, 32];
 //   console.log(`Users marks are ${studentMarks}`);
 // }
 
-console.log(null == 0);
+// console.log(null == 0);
+
+const map = new Map();
+
+map.set("IND", "India");
+map.set("JP", "JAPAN");
+map.set("IN", "India");
+
+// console.log(map);
+
+for (const [key, value] of map) {
+  // console.log(key, " :- ", value);
+}
+
+const myobj = {
+  // name: "akshay",
+  // age: 20,
+  // education: mca,
+
+  IN: "India",
+  JP: "Japan",
+  IND: "Indonesia",
+};
+
+// for (const obj of myobj) {
+//   console.log(obj);
+// }
+
+// for (const keys in myobj) {
+//   console.log(`${keys} is short formm of ${myobj[keys]}`);
+// }
+
+const format = [
+  {
+    username: "abcde",
+    email: "efgh@gmail.com",
+    password: "12345",
+  },
+  {
+    username: "abde",
+    email: "efXL@gmail.com",
+    password: "12345",
+  },
+  {
+    username: "DDEX",
+    email: "FFOgh@gmail.com",
+    password: "12345",
+  },
+];
+
+format.forEach((item) => {
+  // console.log(item.email);
+});
+
+hello(goodbye);
+
+function hello(callback) {
+  setTimeout(() => {
+    console.log("hello");
+    callback();
+  }, 3000);
+}
+
+function goodbye() {
+  console.log("goodbye");
+}
